96c95064032f7cd1b098b7851badfc8429037fde
Neo <32625025+StephenNeo@users.noreply.github.com>
2019-04-18 14:33:13 +0800

    Add files via upload
    v1 w/o priority_table

---
+++ project1-reversi/README.txt
@@ -0,0 +1,6 @@
+Write your own engines using MCTS.
+
+Example:
+python reversi.py -a greedy -b random
+python reversi.py -a greedy -b random -t 30 -v
+python reversi.py -a human -b greedy -t 30
---
+++ project1-reversi/engines/MCTS.py
@@ -0,0 +1,212 @@
+import time
+from engines import Engine
+from random import choice
+from copy import deepcopy
+from math import log, sqrt
+
+
+class Node(object):
+    """
+    This class represents the tree node in a Monte-Carlo tree.
+    """
+    def __init__(self, board, color, depth, parent=None, premove=None):
+        """
+        :param board: the current board
+        :param color: the current moving color
+        :param depth: depth of the node in the search tree
+        :param parent: the parent node
+        :param premove: the last move
+        """
+        self.board = board
+        self.color = color
+        self.depth = depth
+        self.parent = parent
+        self.premove = premove
+        # Number of visits
+        self.visit_num = 0
+        # Valid moves
+        self.remain_valid_moves = board.get_legal_moves(color)
+        # Children of current node
+        self.children = []
+        # N: Visit times
+        self.N = 0
+        # Q: Total reward, stands for the number of white wins
+        self.Q = 0
+
+    def add_child(self, board, new_move):
+        # Append a new node to the current one
+        self.remain_valid_moves.remove(new_move)
+
+        new_board = deepcopy(board)
+        new_board.execute_move(new_move, self.color)
+        new_node = Node(new_board, -self.color, self.depth + 1, self, new_move)
+        self.children.append(new_node)
+
+        return new_node
+
+    def is_terminal(self):
+        # Check if the node is a terminal one
+        return not self.board.get_legal_moves(self.color)
+
+    def is_fully_expanded(self):
+        # Check if the node is fully expanded
+        if not self.remain_valid_moves:
+            return True
+        else:
+            return False
+    
+
+class MctsEngine(Engine):
+    """
+    This game engine uses UCT search method to look for the best
+    move of a given condition.
+    """
+    def __init__(self):
+        self.move_num = 0
+        self.comp_budget = 2
+        self.root = None
+        # Parameter for MCTS
+        self.Cp = 1
+
+    def get_move(self, board, color, move_num=None,
+                 time_remaining=None, time_opponent=None):
+        # Get the root for the search tree
+        self.root = Node(board, color, 1)
+        # If there are corner moves, return them
+        if (0, 0) in self.root.remain_valid_moves:
+            return (0, 0)
+        if (0, 7) in self.root.remain_valid_moves:
+            return (0, 7)
+        if (7, 0) in self.root.remain_valid_moves:
+            return (7, 0)
+        if (7, 7) in self.root.remain_valid_moves:
+            return (7, 7)
+        # Get the move number
+        self.move_num = move_num
+
+        # Get the total time
+        total_time = self.comp_budget
+        while total_time > 0:
+            start_time = time.time()
+            
+            # Get the node to expand
+            new_node = self.tree_policy(self.root)
+            # Expand the node and get the reward
+            reward = self.default_policy(new_node)
+            # print(reward)
+            # print(total_time)
+            # Use reward to change N and Q value of previous nodes
+            self.backup(new_node, reward)
+
+            end_time = time.time()
+            interval = end_time - start_time
+            # print(interval)
+            # print()
+            total_time -= interval
+        
+        # Return bestchild
+        v, node = self.best_child(self.root)
+        return node.premove
+    
+    def tree_policy(self, node):
+        # Look for a new node to expand
+        while not node.is_terminal():
+            if not node.is_fully_expanded():
+                return self.expand(node)
+            else:
+                v, node = self.best_child(node)
+        return node
+
+    def is_terminal(self, board):
+        # Check if the board is a terminal one
+        return not board.get_legal_moves(1) and not board.get_legal_moves(-1)
+
+    def terminal_reward(self, board):
+        # white wins -> 1
+        # black wins -> 0
+        # tie -> 0.5
+        # reward = 0
+        # for line in board.pieces:
+        #     for entry in line:
+        #         reward += entry
+        reward = sum(map(sum, board.pieces))
+        if reward > 0:
+            return 1
+        elif reward < 0:
+            return 0
+        else:
+            return 0.5
+
+    def default_policy(self, node):
+        # Get the reward of the given node
+        cur_reward = 0
+        cur_color = node.color
+        cur_move_num = self.move_num
+        cur_board = deepcopy(node.board)
+        remaining_num = 8
+
+        while remaining_num > 0:
+            while cur_move_num < 64:
+                # Check corner number
+                corner_num = cur_board.pieces[0][0] + cur_board.pieces[0][7] + cur_board.pieces[7][0] + cur_board.pieces[7][7]
+                if corner_num >= 1:
+                    # White possesses more corner nodes
+                    cur_reward += 1
+                    break
+                elif corner_num <= -1:
+                    # Black possesses more corner nodes
+                    break
+
+                legal_set = cur_board.get_legal_moves(cur_color)
+                if not legal_set:
+                    cur_reward += self.terminal_reward(cur_board)
+                    break
+
+                new_move = choice(legal_set)
+                cur_board.execute_move(new_move, cur_color)
+                cur_reward += self.terminal_reward(cur_board)
+                cur_color *= -1
+                cur_move_num += 1
+                
+            # Reset the game
+            cur_color = node.color
+            cur_move_num = self.move_num
+            cur_board = deepcopy(node.board)
+            remaining_num -= 1
+        
+        # Return the final reward
+        return cur_reward
+
+    def backup(self, node, reward):
+        # Use reward to change N and Q value of previous nodes
+        while node:
+            node.N += 1
+            node.Q += reward
+            node = node.parent
+
+    def expand(self, node):
+        # Expand the node to get an unvisited node
+        moves = node.remain_valid_moves
+        move = choice(moves)
+        new_node = node.add_child(node.board, move)
+        return new_node
+
+    def best_child(self, node):
+        # Look for the node with biggest reward
+        if node.color == 1:
+            # Color is white
+            child_value = [(child.Q/child.N + self.Cp*sqrt(2*log(node.N)/child.N))
+                            for child in node.children]
+        elif node.color == -1:
+            # Color is black
+            child_value = [(1 - child.Q/child.N + self.Cp*sqrt(2*log(node.N)/child.N))
+                            for child in node.children]
+        else:
+            return 0, None
+
+        value = max(child_value)
+        idx = child_value.index(value)
+        return value, node.children[idx]
+
+
+engine = MctsEngine
